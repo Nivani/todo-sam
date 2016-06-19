f339c50f4bc3acaaa624b4140a5515e2ad3926ef
Nick Van Nieuwenhuyse <nick.van.nieuwenhuyse@telenet.be>
2016-06-19 22:44:34 +0200

    Remove unnecessary code and dependencies.

--- system.conf.js
+++ system.conf.js
@@ -9,11 +9,7 @@ System.config({
 System.config({
     paths: {
         "systemjs": "node_modules/systemjs/dist/system.js",
-        "incremental-dom": "node_modules/incremental-dom/dist/incremental-dom.js",
-        "plugin-babel": "node_modules/systemjs-plugin-babel/plugin-babel.js",
-        "systemjs-babel-build": "node_modules/systemjs-plugin-babel/systemjs-babel-browser.js",
-        "system-polyfills": "node_modules/systemjs/dist/system-polyfills.js",
-        "es6-module-loader": "node_modules/es6-module-loader/dist/es6-module-loader.js"
+        "incremental-dom": "node_modules/incremental-dom/dist/incremental-dom.js"
     },
-    transpiler: "plugin-babel"
+    transpiler: "none"
 });
